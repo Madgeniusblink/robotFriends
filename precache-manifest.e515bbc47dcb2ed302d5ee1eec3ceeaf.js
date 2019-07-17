766d7055537a1c3e08734e3287c59703b2c9b3b6
Madgeniusblink <madgeniusblink@gmail.com>
2019-07-16 21:54:23 -0700

    Updates

---
+++ precache-manifest.e515bbc47dcb2ed302d5ee1eec3ceeaf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde9a916667e001ac371c781defbe959",
+    "url": "/robotFriends/index.html"
+  },
+  {
+    "revision": "763dc16c30accf0eb714",
+    "url": "/robotFriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ee282c3eecdaecbd600e",
+    "url": "/robotFriends/static/css/main.e1821f7b.chunk.css"
+  },
+  {
+    "revision": "763dc16c30accf0eb714",
+    "url": "/robotFriends/static/js/2.00212eb8.chunk.js"
+  },
+  {
+    "revision": "ee282c3eecdaecbd600e",
+    "url": "/robotFriends/static/js/main.b4048054.chunk.js"
+  },
+  {
+    "revision": "ccca6d208c081d798745",
+    "url": "/robotFriends/static/js/runtime~main.25c2f457.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robotFriends/static/media/logo.5d5d9eef.svg"
+  }
+]);
