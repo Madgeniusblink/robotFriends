379bf90ea77aaf4bafe6560997603f56c60b7b22
Madgeniusblink <madgeniusblink@gmail.com>
2019-07-31 13:22:38 -0700

    improved lighthouse score

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Card = ({name, email, id, set}) => {
     return (
         <div className="tc bg-purple dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img alt='robots' src={`https://robohash.org/${id}?${set}&100x100`} ></img>
+            <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} ></img>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
