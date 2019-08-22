8f56d9a136c392b6014236f9671ef20c672d7159
Madgeniusblink <madgeniusblink@gmail.com>
2019-08-22 13:16:37 -0700

    Update title

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
         return false
     }
     render() {
-        return <h1 className='f1'>RoboFriends</h1>
+        return <h1 className='f1'>RoboFriends APP</h1>
     }
 }
 
